9ad2cc3ad689e8883561ac4bea0bd6fd0fe4cdf0
Neo Jay Andong <neo.andong@springvalley.tech>
2019-10-29 11:16:27 +0800

    Updates

---
+++ precache-manifest.b59d97047c32f8bd4909ef7152984b1b.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae88c0d8992cfa0d32f081cfaf6da8bd",
+    "url": "/neojayandong/index.html"
+  },
+  {
+    "revision": "fcb40df2a8003980e051",
+    "url": "/neojayandong/static/css/2.3655fe6d.chunk.css"
+  },
+  {
+    "revision": "f8501fd8b8168ba6870f",
+    "url": "/neojayandong/static/css/main.9ab68289.chunk.css"
+  },
+  {
+    "revision": "fcb40df2a8003980e051",
+    "url": "/neojayandong/static/js/2.073e0a83.chunk.js"
+  },
+  {
+    "revision": "f8501fd8b8168ba6870f",
+    "url": "/neojayandong/static/js/main.081cffae.chunk.js"
+  },
+  {
+    "revision": "83f74e22aa090e7ab820",
+    "url": "/neojayandong/static/js/runtime-main.80c9b1db.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/neojayandong/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/neojayandong/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/neojayandong/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/neojayandong/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/neojayandong/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/neojayandong/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/neojayandong/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/neojayandong/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/neojayandong/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/neojayandong/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/neojayandong/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/neojayandong/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/neojayandong/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/neojayandong/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/neojayandong/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "bd519feb7073282bd41a3027b69872fe",
+    "url": "/neojayandong/static/media/happypigs_sc.bd519feb.png"
+  },
+  {
+    "revision": "217451c73ae9522702e53850ab186517",
+    "url": "/neojayandong/static/media/iprotech_sc.217451c7.png"
+  },
+  {
+    "revision": "9ac516e0ab10eb4717a185fd43aaa994",
+    "url": "/neojayandong/static/media/linecare_sc.9ac516e0.png"
+  },
+  {
+    "revision": "117fa487fcfbf7a2a583c48c89835522",
+    "url": "/neojayandong/static/media/me.117fa487.jpg"
+  },
+  {
+    "revision": "ed46f1d6064d44f309f6da7deee62f20",
+    "url": "/neojayandong/static/media/nexus_sc.ed46f1d6.png"
+  },
+  {
+    "revision": "dbd82ac55e300644665069b633a084b9",
+    "url": "/neojayandong/static/media/pilantropo_sc.dbd82ac5.jpg"
+  },
+  {
+    "revision": "f618706c3bcde3e54a31663858e6f711",
+    "url": "/neojayandong/static/media/sitiouno_sc.f618706c.jpg"
+  },
+  {
+    "revision": "5ae45300f6ba04bb4ed932720a9fe890",
+    "url": "/neojayandong/static/media/sv_sc.5ae45300.png"
+  },
+  {
+    "revision": "c00e1949db3ec69dd639f900547bc87d",
+    "url": "/neojayandong/static/media/white_keyboard.c00e1949.png"
+  }
+]);
